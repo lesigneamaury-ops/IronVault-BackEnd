eeab6391c4d74c949caf15c3ad3231dbb2d89a1c
Amaury Lesigne <lesigne.amaury@gmail.com>
2026-01-26 09:45:54 +0100

    cloudinary start

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,19 +1,7 @@
 const router = require("express").Router();
 const UserModel = require("../models/User.model");
 const { isAuthenticated } = require("../middlewares/jwt.middleware");
-
-// router.get("/me", isAuthenticated, (req, res) => {
-//   const userId = req.payload._id;
-//   UserModel.findById(userId)
-//     .then((user) => {
-//       console.log("User retrieved:", user);
-//       res.status(200).json(user);
-//     })
-//     .catch((error) => {
-//       console.log("Error retrieving user:", error);
-//       res.status(500).json({ errorMessage: error });
-//     });
-// });
+const uploader = require("../middlewares/cloudinary.config");
 
 router.get("/me", isAuthenticated, async (req, res, next) => {
   try {
@@ -29,17 +17,45 @@ router.get("/me", isAuthenticated, async (req, res, next) => {
 router.patch("/me", isAuthenticated, async (req, res, next) => {
   try {
     const userId = req.payload._id;
-    const { profilePicture, soclialLinks } = req.body;
+
+    const { userName, email, profilePicture, socialLinks } = req.body;
+
     const updatedUser = await UserModel.findByIdAndUpdate(
       userId,
-      { profilePicture, soclialLinks },
-      { new: true },
+      {
+        userName,
+        email,
+        profilePicture,
+        socialLinks,
+      },
+      { new: true, runValidators: true },
     ).select("-passwordHash");
-    console.log("User updated:", updatedUser);
+
     res.status(200).json(updatedUser);
   } catch (error) {
     next(error);
   }
 });
+
+router.post(
+  "/me/profile-picture",
+  isAuthenticated,
+  uploader.single("image"),
+  async (req, res, next) => {
+    try {
+      const userId = req.payload._id;
+
+      const updatedUser = await UserModel.findByIdAndUpdate(
+        userId,
+        { profilePicture: req.file.path },
+        { new: true },
+      ).select("-passwordHash");
+
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 module.exports = router;
